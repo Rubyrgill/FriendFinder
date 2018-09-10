6bc1d937b35cf0fbfded2fda2f59ef3d9550bc8a
RubyG <rubyrgill@hotmail.com>
2018-09-10 17:44:50 -0400

    server tested

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("bye")
+// Dependencies Connecting 
+var express = require("express");
+var bodyparser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+//Port 
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.listen(PORT, function () {
+    console.log('Running on PORT ' + PORT);
+});
+
