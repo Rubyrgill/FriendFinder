e64218f538a4b2e49bbc4f1b609301d3bff9d818
RubyG <rubyrgill@hotmail.com>
2018-09-10 18:08:19 -0400

    set up friendlist array

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+        "name": "Bob",
+        "Photo": "https://is4-ssl.mzstatic.com/image/thumb/Purple111/v4/50/0b/3b/500b3b8f-b72d-b4b0-d487-d317ed24914e/pr_source.png/246x0w.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -8,9 +8,17 @@ var app = express();
 //Port 
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+
+//Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World');
+// });
 
 app.listen(PORT, function () {
     console.log('Running on PORT ' + PORT);
